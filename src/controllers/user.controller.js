f62cc127715d69878bd2a267c527bd87cbb16bb9
BOSS2245Y <144450334+BOSS2245Y@users.noreply.github.com>
2025-01-01 19:28:36 +0530

    j

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -100,7 +100,7 @@ const loginUser = asyncHandler(async(req,res) => {
 
     const options = {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: 'None'
 
     }
@@ -133,7 +133,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: 'None'
 
     }
